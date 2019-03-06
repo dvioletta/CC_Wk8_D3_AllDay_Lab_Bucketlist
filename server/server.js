ec3223211b3c097bbc274433fde42d5b11792d4a
dvioletta <Anita@Pixie.local>
2019-03-06 11:13:07 +0000

    completed form for submitting

--- server/server.js
+++ server/server.js
@@ -3,3 +3,10 @@ const app = express();
 const path = require('path');
 const MongoClient = require('mongodb').MongoClient;
 const createRouter = require('./helpers/create_router.js');
+
+const publicPath = path.join(__dirname, '../client/public');
+app.use(express.static(publicPath));
+
+app.listen(3000, function(){
+  console.log(`Listen on port ${ this.address().port}`);
+})
