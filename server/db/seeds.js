2800404f3969b7dff870bb5bca10105e2f4be093
dvioletta <Anita@Pixie.local>
2019-03-06 13:38:18 +0000

    added seeds to Mongo

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -1,2 +1,15 @@
 use bucket;
 db.dropDatabase();
+
+db.bucket.insertMany([
+  {
+    name: "See London",
+    description: "See all the tourist things in London",
+    date:"2016/01/01"
+  },
+  {
+    name:"Climb a Mountain",
+    description: "Find a Munro to climb",
+    date:"2020/12/12"
+  }
+]);
