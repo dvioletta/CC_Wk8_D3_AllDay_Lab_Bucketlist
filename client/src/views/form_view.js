a78ee0363da8ad757a13d5e3bd3e04ec10ee11f0
dvioletta <Anita@Pixie.local>
2019-03-06 11:06:01 +0000

    adding form details

--- client/src/views/form_view.js
+++ client/src/views/form_view.js
@@ -1,6 +1,23 @@
 const Bucket = require('../models/bucket');
 
 
-const FormView = function() {
-  this.container = container
+const FormView = function(form) {
+  this.form = form
+}
+
+FormView.prototype.bindevents = function() {
+  this.form.addEventListener('submit', (evt) => {
+    this.handleSubmit(evt);
+  });
+
+
+Need to write in here a handlesubmit event.
+Need to write in here create form.
+
+FormView.prototype.createFormBucket = function() {
+  const newBucket = {
+    name: form.name.value,
+    description: form.description.value,
+    date: form.date.value
+  }
 }
